1bbed2cdfeaec60a77e77ad2fd19c6afef02fc07
Dariusz Urbanczyk <dariuszurbanczyk@dariuszs-macbook-air.local>
2019-03-04 10:59:12 +0000

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Dariusz Urbanczyk on 04/03/2019.
+//  Copyright © 2019 Dariusz Urbanczyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
